4141d0d2c936d8c2ec6d608f9ea26595b1036809
Nguyen Thanh Luan <nguyen.thanh.luan@framgia.com>
2016-07-13 10:38:34 +0700

    init

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,81 @@
+class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+
+  # GET /locations
+  # GET /locations.json
+  def index
+    lat = params[:lat].to_f
+    long = params[:long].to_f
+    long, lat = 135.438250, 34.789808 unless (long.present? && lat.present?)
+    @client = GooglePlaces::Client.new('AIzaSyBQ-uK6ziccndcWDdOhE8tmZ3dI4A5AtH8')
+ 
+    @locations = @client.spots(lat, long, types: "train_station", rankby: "distance",language: 'ja')
+    # @locations = @client.spots_by_query("train_station in Hokkaido", language: 'ja') 
+
+  end
+
+  # GET /locations/1
+  # GET /locations/1.json
+  def show
+  end
+
+  # GET /locations/new
+  def new
+    @location = Location.new
+  end
+
+  # GET /locations/1/edit
+  def edit
+  end
+
+  # POST /locations
+  # POST /locations.json
+  def create
+    @location = Location.new(location_params)
+
+    respond_to do |format|
+      if @location.save
+        format.html { redirect_to @location, notice: 'Location was successfully created.' }
+        format.json { render :show, status: :created, location: @location }
+      else
+        format.html { render :new }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /locations/1
+  # PATCH/PUT /locations/1.json
+  def update
+    respond_to do |format|
+      if @location.update(location_params)
+        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+        format.json { render :show, status: :ok, location: @location }
+      else
+        format.html { render :edit }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /locations/1
+  # DELETE /locations/1.json
+  def destroy
+    @location.destroy
+    respond_to do |format|
+      format.html { redirect_to locations_url, notice: 'Location was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_location
+      @location = Location.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def location_params
+      params.require(:location).permit(:address, :latitude, :longitude)
+    end
+end
